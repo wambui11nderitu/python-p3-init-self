b24ae7a42a03522268d6be33654658d4b845103e
Wambui <nderituwambui93@gmail.com>
2024-06-14 23:08:50 +0300

    updating labs

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        '''takes a name as an argument and saves it to self.name'''
+        self.name = name
+        self.breed = breed
